727ed4325b01a7a5f898e04e204f3143bcd12903
Aloshkya <alexis.belyakoff@yandex.ru>
2025-08-19 22:37:48 +0300

    added Docker with Chrome 138

--- run-tests.sh
+++ run-tests.sh
@@ -2,18 +2,13 @@
 set -e
 
 cd /workspace
-export DISPLAY=:99
 
-# Запуск Xvfb
-Xvfb :99 -screen 0 1366x768x24 &
-sleep 3
+# Запуск виртуального дисплея
+Xvfb :99 -screen 0 1366x768x24 & sleep 3
 
-# Запуск записи экрана
-mkdir -p target/allure-results
+# Старт записи экрана
 ffmpeg -y -video_size 1366x768 -framerate 15 -f x11grab -i :99 \
-  -codec:v libx264 -pix_fmt yuv420p -movflags +faststart \
-  target/allure-results/screen_recording.mp4 > ffmpeg.log 2>&1 &
-echo $! > ffmpeg_pid.txt
+  -codec:v libx264 -pix_fmt yuv420p screen_recording.mp4 > /dev/null 2>&1 & echo $! > ffmpeg_pid.txt
 
 # Запуск Maven-тестов
 mvn -B clean test -Dgroups=Second -DsuiteXmlFile='src/test/resources/StartNodes.xml' \
@@ -30,9 +25,9 @@ mvn -B clean test -Dgroups=Second -DsuiteXmlFile='src/test/resources/StartNodes.
     -DSEED_PHRASE_1="$DSEED_PHRASE_1" -DEMAIL_1="$DEMAIL_1" \
     -DSEED_PHRASE_0="$DSEED_PHRASE_0" -DEMAIL_0="$DEMAIL_0"
 
-# Генерация Allure-отчета
-allure generate target/allure-results --clean -o target/allure-report
+# Остановка записи
+kill -INT $(cat ffmpeg_pid.txt) && sleep 2
 
-# Остановка ffmpeg
-kill -INT $(cat ffmpeg_pid.txt) || true
-sleep 5
+# Сохраняем видео для Allure
+mkdir -p target/allure-results
+cp screen_recording.mp4 target/allure-results/
