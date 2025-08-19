19391fcfd9f49f1b1fbc27124e79a779a6ea4986
alexisbelyakoff-sys <alexis.belyakoff@gmail.com>
2025-08-19 22:32:57 +0300

    Delete src directory

--- src/test/resources/extensions/HaHaWallet/scripts/phising-detector.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-window.addEventListener("message", (event) => {
-    if (event.source !== window) return;
-
-    const { type, domain } = event.data;
-    if (type === "bypass-domain") {
-        chrome.runtime.sendMessage({ type: "bypass-domain", domain });
-    }
-});
